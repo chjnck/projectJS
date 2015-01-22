019516e66678e69cb3d2ad2aae9cfdc29c860dd1
irmina90 <s362704@skaner-lib.labs.wmi.amu.edu.pl>
2015-01-22 14:58:43 +0100

    Grunt modules and bootstrap files

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,44 @@ module.exports = function(grunt) {
         jshint: {
             files: ['src/**/*.js', 'test/**/*.spec.js'],
             options: {
-            // options here to override JSHint defaults
-            jshintrc: true
-            // TODO
+                // options here to override JSHint defaults
+                jshintrc: true
+            }
+        },
+        less: {
+          development: {
+              options: {
+                  paths: ["assets/"]
+              },
+              files: {"css/main.css": "less/main.less"}
+          },
+          production: {
+              options: {
+                  paths: ["assets/"]
+              },
+              files: {"css/main.css": "less/main.less"}
+          }
+        },
+        injector: {
+            options: {},
+                local_dependencies: {
+                files: {
+                'index.html': ['**/*.js', '**/*.css']
+                }
+            }
+        },
+        watch: {
+            files: ['**/*'],
+            tasks: ['jshint']
         }
-    }
-
   });
 
-  // Load the plugin that provides the "jshint" task.
-     grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Load the plugin
+     grunt.loadNpmTasks('grunt-contrib-jshint'); // detect errors and potential problems in JavaScript code
+     grunt.loadNpmTasks('grunt-injector');       // inject references to files into other files
+     grunt.loadNpmTasks('grunt-contrib-less');   // CSS pre-processor
+     grunt.loadNpmTasks('grunt-contrib-watch');  // Simple config to run jshint any time a file is added, changed or deleted
+
+
+     grunt.registerTask('all', ['jshint', 'less', 'injector', 'watch']);
 };
