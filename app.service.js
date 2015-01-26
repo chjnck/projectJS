f379980dcf7155d6030e8fd1a6908d66119845e7
irmina90 <weronika.zietek@gmail.com>
2015-01-26 23:16:52 +0100

    get emails inbox ctrl

--- app.service.js
+++ app.service.js
@@ -10,24 +10,4 @@ angular.module('app')
                 });
             }
         };
-}]);
-
-
-
-
-/*angular.module('app')
-    .factory('emails', ['$http', function ($http) {
- emails.get = function(callback) {
- if (!emails) {
- return $http.get('/emails').then(function(response) {
- emails = response.data;
- $rootScope.$broadcast('recivedEmails',emails);
- return emails;
- }).then(callback)
- } else {
- }
- };
-
- return emails;
     }]);
-*/
--- modules/inbox/inbox.controller.js
+++ modules/inbox/inbox.controller.js
@@ -1,17 +1,42 @@
 angular.module('app')
     .controller('emailsCtrl', ['$scope', '$rootScope', 'localStorageService', 'emails', function ($scope, $rootScope, localStorageService, emails) {
-        $scope.emails = localStorageService.get('localEmails');
-        $scope.source = 'Local';
-        if ($scope.emails === null) {
-            console.log('http required');
-            emails.getEmails().then(function(response){
-                console.log('w srodku');
-                $scope.emails = response;
-                $rootScope.$broadcast('recivedEmails',$scope.emails);
-                localStorageService.add('localEmails',$scope.emails);
-                $scope.source = 'Online';
+        var emailsList = {};
+
+        var sortEmails = function(data) {
+
+        };
+
+        var length = 0;
+        var localStorage = function() {
+            $scope.emails = localStorageService.get('localEmails');
+            $scope.source = 'Local';
+            if ($scope.emails === null) {
+                console.log('http required');
+                emails.getEmails().then(function(response){
+                    $scope.emails = response;
+                    //$rootScope.$broadcast('recivedEmails',$scope.emails);
+                    localStorageService.add('localEmails',$scope.emails);
+                    length = $scope.emails.length;
+                    $scope.source = 'Online';
+                });
+            } else {
+                console.log('On local storage. Nothing do be done.');
+            }
+        };
+
+
+        var load = function() {
+            emails.getEmails().then(function(response) {
+                    $scope.emails = response;
+                    console.log($scope.emails[1].received);
+                    var newLength = $scope.emails.length;
+                    if (newLength > length) {
+
+                    }
             });
-        } else {
-            console.log('On local storage. Nothing do be done.');
-        }
+        };
+
+        localStorage(); // load data from localStorage
+        setInterval(load,5000); // every 5 sec
+
     }]);
